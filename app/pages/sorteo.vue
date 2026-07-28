b7205c1e1512c096f0be9688aa7348b653f81a01
Ahmed Rangel <ahmedrangel@outlook.com>
2026-07-27 22:37:19 -0500

    chore: adjusts

--- app/pages/sorteo.vue
+++ app/pages/sorteo.vue
@@ -2,35 +2,35 @@
 const data = ref([
   {
     tipo: "Sub tier 1",
-    oportunidad: "1 oportunidad de ganar."
+    oportunidad: "1 oportunidad"
   },
   {
     tipo: "Sub tier 2",
-    oportunidad: "4 oportunidades de ganar."
+    oportunidad: "4 oportunidades"
   },
   {
     tipo: "Sub tier 3",
-    oportunidad: "9 oportunidades de ganar."
+    oportunidad: "9 oportunidades"
   },
   {
     tipo: "Canjeo puntos (500.000)",
-    oportunidad: "máximo 1 por persona = 1 oportunidad de ganar."
+    oportunidad: "máximo 1 por persona = 1 oportunidad"
   },
   {
     tipo: "Donadores de subs",
-    oportunidad: "Tier 1 = 1 oportunidad de ganar.\nTier 2 = 4 oportunidades de ganar.\nTier 3 = 9 oportunidades de ganar."
+    oportunidad: "Tier 1 = 1 oportunidad\nTier 2 = 4 oportunidades\nTier 3 = 9 oportunidades"
   },
   {
     tipo: "Donadores de bits (1000 bits)",
-    oportunidad: "1 oportunidad de ganar."
+    oportunidad: "1 oportunidad"
   },
   {
     tipo: "Donadores directos (10 USD)",
-    oportunidad: "1 oportunidad de ganar."
+    oportunidad: "1 oportunidad"
   },
   {
     tipo: "Los primos (60 meses o más suscritos)",
-    oportunidad: "2 oportunidades de ganar."
+    oportunidad: "2 oportunidades"
   }
 ]);
 </script>
@@ -47,7 +47,7 @@ const data = ref([
     </section>
     <aside class="md:w-2/4 w-full bg-neutral-950/75 border border-slate-400/40 rounded p-6" aria-labelledby="bases-title">
       <h2 class="text-base mb-2"><span class="font-semibold">FECHA:</span> 9 de agosto, 2026</h2>
-      <h2 class="text-lg uppercase font-semibold mb-2">Bases</h2>
+      <h2 class="text-lg uppercase font-semibold mb-2">Bases para ganar</h2>
       <div class="overflow-x-auto">
         <UTable
           :data="data"
