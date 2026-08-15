b8a6aaa2574834b53949acf056215d7508d48878
Yizack Rangel <yizackr@gmail.com>
2026-08-15 04:43:06 +0200

    refactor(header): extract header toggler to component

--- app/components/SiteHeader.vue
+++ app/components/SiteHeader.vue
@@ -2,42 +2,11 @@
 const { pages: navPages, bodyPages } = await useNavbar();
 
 const subHeader = ref(false);
-const subHeaderPointerEvents = ref(false);
-const toggleSubHeader = () => {
-  subHeader.value = !subHeader.value;
-  if (subHeaderPointerEvents.value) {
-    subHeaderPointerEvents.value = false;
-    return;
-  }
-  setTimeout(() => {
-    subHeaderPointerEvents.value = true;
-  }, 600);
-};
+const pointerEvents = ref(false);
 </script>
 
 <template>
-  <!-- center div -->
-  <div
-    class="fixed flex justify-center z-1 transition-all"
-    :class="subHeader ? 'top-24.5 hover:top-26' : 'top-12.5 hover:top-14 duration-200'"
-  >
-    <UButton
-      v-if="subHeader"
-      class="rounded-t-none rounded-b-full"
-      icon="lucide:chevron-up"
-      :ui="{ base: 'py-0 px-3 hover:bg-primary' }"
-      size="sm"
-      @click="toggleSubHeader"
-    />
-    <UButton
-      v-else
-      class="rounded-t-none rounded-b-full"
-      icon="lucide:chevron-down"
-      :ui="{ base: 'py-0 px-3 hover:bg-primary' }"
-      size="sm"
-      @click="toggleSubHeader"
-    />
-  </div>
+  <SiteHeaderToggler v-model:sub-header="subHeader" v-model:pointer-events="pointerEvents" />
   <UHeader>
     <template #title>
       <div class="flex items-center gap-2">
@@ -80,7 +49,7 @@ const toggleSubHeader = () => {
   </UHeader>
   <UHeader
     class="transition-all duration-200"
-    :class="[subHeader ? 'top-14 h-12' : 'h-0 -z-1 opacity-0', { 'pointer-events-none': !subHeaderPointerEvents }]"
+    :class="[subHeader ? 'top-14 h-12' : 'h-0 -z-1 opacity-0', { 'pointer-events-none': !pointerEvents }]"
     :toggle="false"
   >
     <UNavigationMenu
---
+++ app/components/SiteHeaderToggler.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const subHeader = defineModel("subHeader", { type: Boolean, default: false });
+const pointerEvents = defineModel("pointerEvents", { type: Boolean, default: false });
+
+const toggleSubHeader = () => {
+  subHeader.value = !subHeader.value;
+  if (pointerEvents.value) {
+    pointerEvents.value = false;
+    return;
+  }
+  setTimeout(() => {
+    pointerEvents.value = true;
+  }, 600);
+};
+</script>
+
+<template>
+  <div
+    class="fixed justify-center z-1 transition-all lg:flex hidden"
+    :class="subHeader ? 'top-24.5 hover:top-26' : 'top-12.5 hover:top-14 duration-200'"
+  >
+    <UButton
+      v-if="subHeader"
+      class="rounded-t-none rounded-b-full"
+      icon="lucide:chevron-up"
+      :ui="{ base: 'py-0 px-3 hover:bg-primary' }"
+      size="sm"
+      @click="toggleSubHeader"
+    />
+    <UButton
+      v-else
+      class="rounded-t-none rounded-b-full"
+      icon="lucide:chevron-down"
+      :ui="{ base: 'py-0 px-3 hover:bg-primary' }"
+      size="sm"
+      @click="toggleSubHeader"
+    />
+  </div>
+</template>
