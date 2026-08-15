b7aaeaec5b6b3204749d848b8968e8d554a2ca55
Yizack Rangel <yizackr@gmail.com>
2026-08-15 22:00:19 +0200

    style(site-header): tweak toggler hover positions

--- app/components/SiteHeaderToggler.vue
+++ app/components/SiteHeaderToggler.vue
@@ -19,7 +19,7 @@ const icon = computed(() => showHeader.value ? "lucide:chevron-up" : "lucide:che
 <template>
   <div
     class="fixed lg:flex hidden justify-center z-1 transition-all duration-300"
-    :class="showHeader ? 'top-24.5 hover:top-26' : 'top-12.5 hover:top-14'"
+    :class="showHeader ? 'top-24.5 hover:top-25.5' : 'top-12.5 hover:top-13.5'"
   >
     <div class="group">
       <UButton
